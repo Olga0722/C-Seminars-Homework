bb904bb1727bc4df94427cc751432ca7f248e4d0
olga <o.v.matvienko@yandex.ru>
2022-09-20 15:51:05 +0300

    добавлены задачи с 8-го семинара

---
+++ СЕМИНАР_8/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int [,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows,columns];
+
+    for(int i=0; i<rows; i++)
+    {
+        for (int j=0; j<columns; j++)
+        {
+            newArray[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]+ " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] ReplaceRowsColumns (int[,] array)
+{
+    int[,] newArray=new int [array.GetLength(0), array.GetLength(1)];
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        newArray[j, i] = array[i,j];
+    }
+    return newArray;
+}
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = CreateRandom2dArray(m, n, min, max);
+Show2dArray(array);
+Show2dArray(ReplaceRowsColumns(array));
